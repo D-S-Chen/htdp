fb8851984eaa1c7a2d98d0281a644af7d6dbddeb
D-S-Chen <88534828+D-S-Chen@users.noreply.github.com>
2021-08-06 16:37:05 +0700

    Add files via upload
    Simple task. But I have no idea what planets are in our solar system (lol), so I had to check Wikipedia.

---
+++ htdp-129.rkt
@@ -0,0 +1,19 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname htdp-129) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(cons "Mercury"
+      (cons "Venus"
+            (cons "Earth"
+                  (cons "Mars"
+                        (cons "Jupiter"
+                              (cons "Saturn"
+                                    (cons "Uranus"
+                                          (cons "Neptune" '()))))))))
+
+(cons "bread"
+      (cons "pizza"
+            (cons "kebab" '())))
+
+(cons "red"
+      (cons "green"
+            (cons "blue" '())))
